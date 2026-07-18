65cae32e864b63cebacd334b22a3bf56b9c091a5
João Vinícius <joaovinicius75010@gmail.com>
2026-07-18 18:32:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// CONFIGURAÇÃO: Link oficial para onde o usuário será enviado ao clicar
+const LINK_MELI = "https://meli.la/2acEwUf";
+
+// Atualiza o link do botão dinamicamente na página
+document.getElementById('Meli-link').href = LINK_MELI;
